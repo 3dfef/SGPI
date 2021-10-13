1c6306d9bd21a18139d7fc1c314a062ea9eab538
Yerson Andres Sequea Usme <yerson.sequea@correo.tdea.edu.co>
2021-10-13 09:34:14 -0500

    Version 2.0

---
+++ Criptografia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SGPI
+{
+    public class Criptografia
+    {
+        
+        public string CodigoHash(String textoClaro)
+        {
+
+            using (SHA256 sHA256= new SHA256Managed())
+            {
+
+                byte[] bytes = sHA256.ComputeHash(Encoding.ASCII.GetBytes (textoClaro));
+                stringBuilder
+            }
+        }
+    }
+}
--- inicio.aspx.cs
+++ inicio.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Linq;
 
 namespace SGPI
 {
@@ -16,7 +17,45 @@ namespace SGPI
 
         protected void btnEnviar_Click(object sender, EventArgs e)
         {
-            Response.Redirect("/MenuOpciones.aspx");
+            using (EntitiesDB DBEntities = new EntitiesDB())
+
+                
+            {
+                Usuario usr = new Usuario();
+
+                try {
+                   Usuario usuario = DBEntities.Usuario.FirstOrDefault(a => a.Documento == TxtUsuario.Text && a.Contraseña == TxtContraseña.Text);
+                   
+                   
+                    if (usuario != null)
+                    {
+                        switch (usuario.Rol.IdRol)
+                        {
+                            case 1:
+                                Response.Redirect("~/AdminPrincipal.aspx");
+                                
+                                break;
+                            case 2:
+                                Response.Redirect("~/Estudiante.aspx");
+                                break;
+                            case 3:
+                                Response.Redirect("~/coordinador.aspx");
+                                break;
+                            default:
+                                break;
+
+
+                        }
+                        
+                    }
+                    else
+                        Response.Write("<script>alert('Usuario o contraseña invalidos.Por Favor " + "Verifique Nuevamente')</script>");
+                }
+                catch (Exception except)
+                {
+
+                }
+            }
         }
     }
 }
